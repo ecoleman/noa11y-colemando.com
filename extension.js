291cd025483d9d809cc2fbd231fbcb4db9ffa558
Felipe Morales <hel.sheep@gmail.com>
2011-10-03 18:04:18 -0300

    Should make it compatible with other extensions that modify the status area.

--- extension.js
+++ extension.js
@@ -1,18 +1,20 @@
 const Panel = imports.ui.panel;
 const Main = imports.ui.main;
 
-idx = null;
-
 function enable() {
-    idx = Panel.STANDARD_STATUS_AREA_ORDER.indexOf("a11y");
-	Main.panel._rightBox.get_children()[idx].destroy();
+	for (let i = 0; i < Main.panel._rightBox.get_children().length; i++) {
+		if (Main.panel._statusArea['a11y'] == Main.panel._rightBox.get_children()[i]._delegate) {
+			Main.panel._rightBox.get_children()[i].destroy();
+			break;
+		} 
+	}
 	// addToStatusArea would throw an error on disable if we don't set this to null 
 	Main.panel._statusArea['a11y'] = null;
 }
 
 function disable() {
 	let indicator = new Panel.STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION["a11y"];
-	Main.panel.addToStatusArea("a11y", indicator, idx);
+	Main.panel.addToStatusArea("a11y", indicator, Panel.STANDARD_STATUS_AREA_ORDER.indexOf('a11y'));
 }
 
 function init() {
